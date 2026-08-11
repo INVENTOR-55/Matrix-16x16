1cc6af7bc064e59ea43f5333a052cdaab3db6dfa
INVENTOR 55 <126707371+INVENTOR-55@users.noreply.github.com>
2026-08-11 17:57:32 +0200

    Add files via upload

---
+++ animations/animation_types.h
@@ -0,0 +1,15 @@
+#ifndef ANIMATION_TYPES_H
+#define ANIMATION_TYPES_H
+#include <stdint.h>
+#include <Arduino.h>
+
+typedef struct {
+    const char *name;
+    const uint16_t frameCount;
+    const uint8_t width; 
+    const uint8_t height; 
+    const uint8_t *frameDurations; 
+    const uint16_t *animationFrames; 
+} Animation;
+
+#endif
